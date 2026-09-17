56c4f4ccac38d859b28b74271367c0724b8f9653
Nitish-Mudhiraj <nitishnithin55@gmail.com>
2026-09-17 16:56:15 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -10,11 +10,17 @@ const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
 const app = express()
 app.use(express.json())
 app.use(cookieparser())
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://snitch-e-commercee.onrender.com"
+];
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-app.use(express.static("./public"))
+  origin: allowedOrigins,
+  credentials: true
+}));
+
+
 
 app.use(passport.initialize())
 passport.use(
